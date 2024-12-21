396f10eca4ae32d8d449f87be493f0d214bbab7b
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2024-12-21 19:58:14 +0530

    chore: add supabase package

--- lib/src/modules/authentication/presentation/splash_screen.dart
+++ lib/src/modules/authentication/presentation/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../design_system/themes/base/theme.dart';
 import '../../../design_system/typography/typography.dart';
