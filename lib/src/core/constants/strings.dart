d55d4b9467a451bc4a43dd1ff828be5e72da5bd6
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2025-01-23 20:49:54 +0530

    chore: update string.dart

--- lib/src/core/constants/strings.dart
+++ lib/src/core/constants/strings.dart
@@ -1,4 +1,9 @@
 /// Content used in the app
 abstract class AppStrings {
   static const somethingWentWrong = 'Something went wrong. Please try again later.';
+  static const noInternetConnection =
+      'No internet connection. Please check your connection and try again.';
+
+  // Project
+  static const failedToFetchProjects = 'Failed to fetch projects from the server';
 }
