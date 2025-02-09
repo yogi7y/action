7a750ba4edddc1f79211cf141f64ac224498dcce
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2025-02-09 11:16:48 +0530

    chore(pagination): introduce total property

--- lib/src/core/network/paginated_response.dart
+++ lib/src/core/network/paginated_response.dart
@@ -6,20 +6,22 @@ typedef Cursor = String;
 class PaginatedResponse<T> {
   const PaginatedResponse({
     required this.results,
+    required this.total,
   });
 
   final List<T> results;
+  final int total;
 
   @override
-  String toString() => 'PaginatedResponse(results: $results)';
+  String toString() => 'PaginatedResponse(results: $results, total: $total)';
 
   @override
   bool operator ==(covariant PaginatedResponse<T> other) {
     if (identical(this, other)) return true;
 
-    return other.results == results;
+    return other.results == results && other.total == total;
   }
 
   @override
-  int get hashCode => results.hashCode;
+  int get hashCode => results.hashCode ^ total.hashCode;
 }
