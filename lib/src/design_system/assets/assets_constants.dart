71c80ffb2d44631fcdf94cce8de193785b6aef12
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2025-02-15 18:50:02 +0530

    feat(design): move from svg to font icons

--- lib/src/design_system/assets/assets_constants.dart
+++ lib/src/design_system/assets/assets_constants.dart
@@ -8,35 +8,29 @@ class AssetsV2 {
   static const String _soundsBase = 'assets/sounds';
 
   // Navigation & Actions
-  static const bookmarkAdd = '$_iconsBase/bookmark-add-outlined.svg';
-  static const addTask = '$_iconsBase/add-task-outlined.svg';
-  static const plus = '$_iconsBase/plus-outlined.svg';
-  static const user = '$_iconsBase/user-outlined.svg';
-  static const clock = '$_iconsBase/clock-outlined.svg';
-  static const loader = '$_iconsBase/loader-outlined.svg';
-  static const hammerOutlined = '$_iconsBase/hammer-outlined.svg';
-
-  static const calendarLinesOutlined = '$_iconsBase/calendar-lines-outlined.svg';
-  static const calendarOutlined = '$_iconsBase/calendar-outlined.svg';
-
-  static const tag = '$_iconsBase/tag-outlined.svg';
-
-  static const xmark = '$_iconsBase/xmark.svg';
-  static const chevronDown = '$_iconsBase/chevron-down.svg';
-
-  static const circle = '$_iconsBase/circle.svg';
-  static const circleCheck = '$_iconsBase/circle_check.svg';
-  static const circleDashed = '$_iconsBase/circle_dashed.svg';
-  static const forwardStep = '$_iconsBase/forward_step.svg';
-
-  static const home = '$_iconsBase/home-smile.svg';
-  static const area = '$_iconsBase/area.svg';
-  static const resources = '$_iconsBase/resources.svg';
-  static const search = '$_iconsBase/search.svg';
-
-  static const check = '$_iconsBase/check-bold.svg';
-  static const chevronLeft = '$_iconsBase/chevron-left.svg';
-  static const send = '$_iconsBase/send-alt-2.svg';
+  static const bookmarkAdd = '$_iconsBase/bookmark-add-outlined.svg.vec';
+  static const addTask = '$_iconsBase/add-task-outlined.svg.vec';
+  static const plus = '$_iconsBase/plus-outlined.svg.vec';
+  static const user = '$_iconsBase/user-outlined.svg.vec';
+  static const clock = '$_iconsBase/clock-outlined.svg.vec';
+  static const loader = '$_iconsBase/loader-outlined.svg.vec';
+  static const hammerOutlined = '$_iconsBase/hammer-outlined.svg.vec';
+  static const calendarLinesOutlined = '$_iconsBase/calendar-lines-outlined.svg.vec';
+  static const calendarOutlined = '$_iconsBase/calendar-outlined.svg.vec';
+  static const tag = '$_iconsBase/tag.svg.vec';
+  static const xmark = '$_iconsBase/xmark.svg.vec';
+  static const chevronDown = '$_iconsBase/chevron-down.svg.vec';
+  static const circle = '$_iconsBase/circle.svg.vec';
+  static const circleCheck = '$_iconsBase/circle_check.svg.vec';
+  static const circleDashed = '$_iconsBase/circle_dashed.svg.vec';
+  static const forwardStep = '$_iconsBase/forward_step.svg.vec';
+  static const home = '$_iconsBase/home-smile.svg.vec';
+  static const area = '$_iconsBase/area.svg.vec';
+  static const resources = '$_iconsBase/resources.svg.vec';
+  static const search = '$_iconsBase/search.svg.vec';
+  static const check = '$_iconsBase/check.svg.vec';
+  static const chevronLeft = '$_iconsBase/chevron-left.svg.vec';
+  static const send = '$_iconsBase/send-alt-2.svg.vec';
 
   // Sounds
   static const bubbleBurst1 = '$_soundsBase/bubble_burst_1_min.mp3';
